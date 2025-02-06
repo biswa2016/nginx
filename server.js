3a9c8deb0045ab2262513dcb095d8827e0acc4d3
Biswa Ranjan <biswaranjannayak2016@gmail.com>
2025-02-07 00:27:10 +0530

    commit test

--- server.js
+++ server.js
@@ -8,19 +8,22 @@ const app = express();
 // Set the port
 const port = 3000;
 
-const replicaApp = process.env.APP_NAME
+const APP_NAME = process.env.APP_NAME
+const envName = process.env
 
 // Serve static files like CSS and images
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Route to serve the HTML file
 app.get('/', (req, res) => {
+    console.log('envName',envName);
+    
     res.sendFile(path.join(__dirname, 'index.html'));
-    console.log(`request serveed by node js ${replicaApp}`);
+    console.log(`request served by node js ${APP_NAME}`);
     
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`${replicaApp} is running at http://localhost:${port}`);
+    console.log(`${APP_NAME} is running at http://localhost:${port}`);
 });
